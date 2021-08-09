5601620252e276654fc058a2758dabcf587cd4b4
David Gugea <davidgugea@yahoo.com>
2021-08-09 15:50:34 +0200

    Association Property { subsets }

---
+++ ClassDiagram/AssociationProperty_subsets/Subsets_Built/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Subsets_Built
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    
+    public class A
+    {
+        B b = new B();
+    }
+    public class B
+    {
+        A a = new A();
+    }
+    public class C : A
+    {
+        D d = new D();
+    }
+    public class D : A
+    {
+        C c = new C();
+    }
+}
