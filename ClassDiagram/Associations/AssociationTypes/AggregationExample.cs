9e4dc2f82007dcab2d179888ab3ac3a8989ffd9a
David Gugea <davidgugea@yahoo.com>
2021-08-10 14:36:56 +0200

    Added Aggregation and Composition

---
+++ ClassDiagram/Associations/AssociationTypes/AggregationExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssociationTypes
+{
+    class AggregationExample
+    {
+    }
+
+    class Chair
+    {
+        public int chairNr { get; set; }
+        public Chair(int nr)
+        {
+            this.chairNr = chairNr;
+        }
+    }
+
+    class Restaurant
+    {
+        public List<Chair> chairs;
+
+        public Restaurant()
+        {
+            chairs = new List<Chair>();
+        }
+
+        public void addChair(Chair s)
+        {
+            chairs.Add(s);
+        }
+
+        public Chair removeChair()
+        {
+            Chair returnChair = chairs[chairs.Count-1];
+            chairs.RemoveAt(chairs.Count - 1);
+            return returnChair;
+        }
+    }
+}
---
+++ ClassDiagram/Associations/AssociationTypes/CompositionExample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssociationTypes
+{
+    class CompositionExample
+    {
+    }
+
+    class Leaf
+    {
+        public int leafNr { get; set; }
+        public Leaf(int nr)
+        {
+            leafNr = nr;
+        }
+    }
+
+    class Tree
+    {
+        private List<Leaf> leaves { get; set; }
+        public Tree()
+        {
+            leaves = new List<Leaf>();
+        }
+
+        public void AddLeaf(int nr)
+        {
+            leaves.Add(new Leaf(nr));
+        }
+    }
+}
